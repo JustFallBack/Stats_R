57bd7d52f8b6c017e9cb58b2bf7cbdefdfd1105b
Elliot Pozucek <fall.f777mdk@gmail.com>
2024-03-20 11:28:47 +0100

    Added comment on TP2_Ex1 and 2

--- TP2/PD_TP2_Ex1.R
+++ TP2/PD_TP2_Ex1.R
@@ -1,23 +1,43 @@
+# TP2 - Exercice 1
+# Utilisation de la librairie datasauRus
+
+
 # install.packages("datasauRus")
 library(datasauRus) # Chargement de la librairie datasauRus
 
+
 set1 <- datasaurus_dozen[datasaurus_dozen$dataset=="dino",2:3] # Extraction de la première BDD
+
 set2 <- datasaurus_dozen[datasaurus_dozen$dataset=="star",2:3] # Extraction de la deuxième BDD
 
+
 # Analyse de la première BDD
+
 summary(set1)
+
 sd(set1[,1])
+
 sd(set1[,2])
+
 cor(set1[,1], set1[,2])
 
+
 # Analyse de la deuxième BDD
+
 summary(set2)
+
 sd(set2[,1])
+
 sd(set2[,2])
+
 cor(set2[,1], set2[,2])
 
 boxplot(set1)
+
 boxplot(set2)
 
+
+# Affichage des BDD
 plot(set1[,1], set1[,2], lwd = "2", col = "darkgreen")
-plot(set2[,1], set2[,2], lwd = "2", col = "yellow")
+
+#plot(set2[,1], set2[,2], lwd = "2", col = "yellow") 
--- TP2/PD_TP2_Ex2.R
+++ TP2/PD_TP2_Ex2.R
@@ -1,21 +1,50 @@
 # TP2 - Variables continues
 # Exercice 2 : Loi exponentielle
 
-lambda = 0.1 # Intensité de la loi exponentielle
+# On suppose que la durée d'attente en minutes de la première alerte après 18h dans une entreprise suit une loi exponentielle de paramètre λ = 0.1.
+# On étudie la distribution de la variable aléatoire qui modélise le temps d'attente.
+
+
+# On utilise une méthode qui consiste à simuler des variables uniformes U_n ~ U(0,1) et à appliquer la fonction inverse.
+
+λ = 0.1 # Intensité de la loi exponentielle
+
 n = 100000
+
 U_n = runif(n) # Simulation de n variables uniformes
-X_n = -log(1-U_n)/lambda # fonction inverse
-hist(X_n, prob = T)
-curve(dexp(x, rate = lambda), col ="red", add = TRUE)
 
-# Calcul de la probabilité qu'il n'y ait aucune alerte avant 22h30.
-mean(X_n>30) #Proba empirique
-pexp(30, lambda, lower.tail = FALSE) # Probabilité théorique
+X = -log(1-U_n) / λ # fonction inverse de la loi exponentielle
 
-# Estimation empirique de la probabilité qu'il n'y ait aucune alerte avant 23h.
-mean(X_n>60)
+# Histogramme de la loi exponentielle (empirique)
+hist(X, prob = T, main = paste("Histogramme de la loi exponentielle de paramètre ", lambda), xlab = "Temps d'attente (en minutes)", ylab = "Densité", col = "lightgreen")
 
-# Estimation empirique de la probabilité qu'il n'y ait aucune alerte avant 23h sachant qu'il n'y a aucune alerte avant 22h30.
-X_n_30 <- X_n[which(X_n>30)]
-mean(X_n_30>60)
-pexp(30, lambda, lower.tail = FALSE)
+# Une courbe modélisant la densité théorique
+curve(dexp(X, rate = λ), col ="red", add = TRUE, xname = "X", ylab = "Densité", lwd = 2)
+
+# On voit que la densité empirique est proche de la densité théorique avec n suffisamment grand.
+
+
+# Calcul de la probabilité qu'il n'y ait aucune alerte avant 18h30.
+
+mean(X > 30) # Probabilité empirique
+
+pexp(30, λ, lower.tail = FALSE) # Probabilité théorique
+
+# On voit que la probabilité empirique est proche de la probabilité théorique pour n suffisamment grand.
+
+
+# Calcul de la probabilité qu'il n'y ait aucune alerte avant 19h.
+
+mean(X > 60) # Probabilité empirique.
+
+pexp(60, λ, lower.tail = FALSE) # Probabilité théorique.
+
+# On voit que la probabilité empirique est proche de la probabilité théorique pour n suffisamment grand.
+
+
+# Estimation empirique de la probabilité qu'il n'y ait aucune alerte avant 19h sachant qu'il n'y a aucune alerte avant 18h30.
+
+X_30 = X[which(X > 30)]
+mean(X_30 > 60)
+
+# On observe que la probabilité empirique qu'il n'y ait aucune alerte avant 19h sachant qu'il n'y a aucune alerte avant 18h30 est la même que la probabilité qu'il n'y ait aucune alerte avant 18h30.
